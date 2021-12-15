e7eab68bf5d830ab1c9de9e0fee638971bc5a20f
Kelly Zihan Zhao <48823712+zzh611@users.noreply.github.com>
2021-12-14 23:12:13 -0600

    Update stable.R

--- code/stable_code/stable.R
+++ code/stable_code/stable.R
@@ -10,7 +10,8 @@ library(arrow)
 data <- read_parquet(paste0(args[1],".parquet"), as_tibble = TRUE)
 data1 <- data[which(data$open_time >= '2020-01-01 23:00:00 CST'),]
 data1 <- data1[which(data1$open_time <= '2021-01-01 23:00:00 CST'),]
-variance <- var(data1$open)
+data1$stand <- data1$open/mean(data1$open)
+variance <- var(data1$stand)
 df <- data.frame("variance" = variance, "file" = args[1])
 write.csv(df, paste("csv/",args[1],".csv", sep = ""), row.names = FALSE)
 
